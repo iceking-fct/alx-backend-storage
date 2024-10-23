785e6377fb94cf979672a45a582236bb444c4eb3
iceking-fct <miracleamajama14@gmail.com>
2024-10-23 18:57:34 +0000

    task 1 solved

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,3 +14,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+        def get(self, key: str, fn: callable = None):
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+        If fn is None, the raw value is returned.
+        """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Helper method to retrieve a string from Redis"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Helper method to retrieve an integer from Redis"""
+        return self.get(key, int)
